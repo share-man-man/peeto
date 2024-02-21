5cd11a630fc5cd64b7386bcf74838238dfc09125
舒晓满 <shuxiaoman@DESKTOP-I0U3D87.localdomain>
2024-02-22 01:24:07 +0800

    feat(packages/editor-react): 新增react编辑器

---
+++ packages/editor-react/src/demo/App.tsx
@@ -0,0 +1,5 @@
+const Index = () => {
+  return <div>编辑器</div>;
+};
+
+export default Index;
---
+++ packages/editor-react/vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'PeetoEditorReact',
+      fileName: 'index',
+      formats: ['es', 'umd', 'cjs', 'iife'],
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'react',
+        },
+      },
+    },
+  },
+  plugins: [dts({ entryRoot: './src' }), react()],
+});
